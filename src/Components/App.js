d26627be256f953e003ad309b00d100985606fdc
maddrake220 <83238246+maddrake220@users.noreply.github.com>
2021-05-02 05:11:36 +0900

    #1 Madplix commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Router from "./Router";
+import GlobalStyles from "./GlobalStyles";
+  /* Header폴더에 index파일이 있기 때문에 
+  Header/Header와 같이 사용하지 않아도 된다. */
+class App extends Component {
+  render() {
+    return(
+      <>
+        <Router />
+        <GlobalStyles />
+      </> /* Fragments : 원하는 만큼 컴포넌트를 Return할 수 있게 해준다.*/
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from "../Routes/Home";
+import Search from "../Routes/Search";
+import Tv from "../Routes/Tv";
+import Header from "./Header";
+
+/* Arrow Function 에서 Return을 사용하지 않을 경우 () 를 사용!!! */
+export default () => (
+   <Router>
+        <>
+        <Header />
+        <Switch>
+            <Route path="/" exact="exact" component={Home}/>
+            <Route path="/search" exact="exact" component={Search}/>
+            <Route path="/tv" exact="exact" component={Tv}/>
+            <Route path="/tv/popular" render={()=> "Popular"}/>
+            <Redirect from="*" to="/"/>
+        </Switch>
+        </>
+   </Router>
+    /* Composition : 두 개 이상의 Route를 동시에 랜더링 하는 방식*/
+    /* Redirect : 어느 페이지는 Route에서 매치 되지 않으면 Redirect를 Render */
+    /* Switch : 한 번에 하나의 Route만 render */
+)
