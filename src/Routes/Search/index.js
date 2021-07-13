07b1785197446f2ee2d14ef0825787e0c94989c9
maddrake220 <83238246+maddrake220@users.noreply.github.com>
2021-07-13 21:06:37 +0900

    refactoring

--- src/Routes/Search/index.js
+++ src/Routes/Search/index.js
@@ -1,3 +1,121 @@
-import SearchContainer from "./SearchContainer"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+import { Helmet } from "react-helmet";
+import { multiSearchApi } from "../../API";
 
-export default SearchContainer;
+const Container = styled.div`
+  margin-top: 20px;
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
+
+const SearchTerm = styled.div`
+  font-size: 20px;
+  margin-bottom: 30px;
+  position: relative;
+  color: yellow;
+`;
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [pastSearchTerm, setPastSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchTerm !== "") {
+      searchByTerm(searchTerm);
+    }
+  };
+  const updateTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSearchTerm(value);
+  };
+  const searchByTerm = async () => {
+    try {
+      const {
+        data: { results: searchResults },
+      } = await multiSearchApi.multiSearch(searchTerm);
+      setPastSearchTerm(searchTerm);
+      setLoading(true);
+      setSearchResults(searchResults);
+    } catch {
+      setError("결과를 찾을 수 없습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  return (
+    <Container>
+      <Helmet>
+        <title>Search | Madflix</title>
+      </Helmet>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="검색하고 싶은 영화, TV시리즈를 입력하세요"
+          value={searchTerm}
+          onChange={updateTerm}
+        ></Input>
+      </Form>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {pastSearchTerm ? (
+            <SearchTerm>
+              '{pastSearchTerm}' 전체 검색결과가 {searchResults.length}건
+              있습니다.
+            </SearchTerm>
+          ) : (
+            pastSearchTerm
+          )}
+          {searchResults && searchResults.length > 0 && (
+            <Section title="검색결과">
+              {searchResults.map((result) => (
+                <Poster
+                  key={result.id}
+                  id={result.id}
+                  title={
+                    result.title ? result.title : result.name && result.name
+                  }
+                  imageUrl={result.poster_path}
+                  isMovie={result.media_type === "movie" ? true : false}
+                  rating={result.vote_average}
+                  year={
+                    result.release_date
+                      ? result.release_date.substring(0, 4)
+                      : result.first_air_date &&
+                        result.first_air_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
+          )}
+          {error && <Message color="#e74c3c" text={error} />}
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default Search;
--- src/Routes/Tv/index.js
+++ src/Routes/Tv/index.js
@@ -1,3 +1,107 @@
-import TVContainer from "./TVContainer"
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+import { Helmet } from "react-helmet";
+import { tvApi } from "../../API";
 
-export default TVContainer;
+const Container = styled.div`
+  padding: 40px;
+`;
+const TV = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [topRated, setTopRated] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [airingToday, setAiringToday] = useState([]);
+
+  const getTvDataFromApi = async () => {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      setTopRated(topRated);
+      setPopular(popular);
+      setAiringToday(airingToday);
+    } catch {
+      setError("Tv 데이터를 찾을 수 없습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getTvDataFromApi();
+  }, []);
+  return (
+    <>
+      <Helmet>
+        <title> TV Shows | Madflix</title>
+      </Helmet>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Container>
+          {topRated && topRated.length > 0 && (
+            <Section title="최고 평점 시리즈">
+              {topRated.map((show) => (
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  title={show.name}
+                  imageUrl={show.poster_path}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
+          )}
+          {airingToday && airingToday.length > 0 && (
+            <Section title="오늘 방영 시리즈">
+              {airingToday.map((show) => (
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  title={show.name}
+                  imageUrl={show.poster_path}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
+          )}
+          {popular && popular.length > 0 && (
+            <Section title="인기 시리즈">
+              {popular.map((show) => (
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  title={show.name}
+                  imageUrl={show.poster_path}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
+          )}
+          {error && <Message color="#e74c3c" text={error} />}
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default TV;
